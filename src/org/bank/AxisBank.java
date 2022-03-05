a477153bcab9a7139b23da4148dd65d195641aa2
vanivanitha <vanitha.svs1993@gmail.com>
2022-03-05 17:43:57 +0530

    VANI5191

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,8 @@
+package org.bank;
+
+public class AxisBank {
+public void deposit() {
+	System.out.println("deposit:25000");// TODO Auto-generated method stub
+
+}
+}
---
+++ src/org/bank/BankInfo2.java
@@ -0,0 +1,18 @@
+package org.bank;
+
+public class BankInfo2 extends AxisBank {
+private void savings() {
+	System.out.println("savings:46500");// TODO Auto-generated method stub
+
+}
+private void fixed() {
+	System.out.println("Fixed:50000");// TODO Auto-generated method stub
+
+}
+public static void main(String[] args) {
+	BankInfo2 b=new BankInfo2();
+	b.savings();
+	b.fixed();
+	b.deposit();
+}
+}

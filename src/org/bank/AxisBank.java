e9a0f320e4da2e8cac0cdf16e7a06668374bf78b
vanivanitha <vanitha.svs1993@gmail.com>
2022-03-05 21:43:58 +0530

    VANI5193

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -3,6 +3,6 @@ package org.bank;
 public class AxisBank {
 public void deposit() {
 	System.out.println("deposit:25000");// TODO Auto-generated method stub
-
+System.out.println("First Cloning Project Success");
 }
 }
--- src/org/bank/BankInfo2.java
+++ src/org/bank/BankInfo2.java
@@ -7,7 +7,7 @@ private void savings() {
 }
 private void fixed() {
 	System.out.println("Fixed:50000");// TODO Auto-generated method stub
-
+	System.out.println("First Cloning Project Success");
 }
 public static void main(String[] args) {
 	BankInfo2 b=new BankInfo2();
